ddf4cdeecce2ef3bc61d26115666501a21d75212
Juan Salvatore <juansalvatore@live.com.ar>
2017-11-02 17:32:01 -0300

    Start Again

--- portfolio.php
+++ portfolio.php
@@ -7,90 +7,15 @@
     <link rel="stylesheet" href="styles/portfolio-style.min.css">
   </head>
   <body>
-    <div class="containerBoole">
-      <div class="top">
 
-      </div>
-      <div class="right">
 
-      </div>
-      <div class="bottom">
-
-      </div>
-      <div class="left">
-
-      </div>
-    </div>
-    <div class="containerSudco">
-      <div class="top">
-
-      </div>
-      <div class="right">
-
-      </div>
-      <div class="bottom">
-
-      </div>
-      <div class="left">
-
-      </div>
-    </div>
-    <div class="containerLaNacion">
-      <div class="top">
-
-      </div>
-      <div class="right">
-
-      </div>
-      <div class="bottom">
-
-      </div>
-      <div class="left">
-
-      </div>
-    </div>
-    <div class="containerShortFilm">
-      <div class="top">
-
-      </div>
-      <div class="right">
-
-      </div>
-      <div class="bottom">
-
-      </div>
-      <div class="left">
-
-      </div>
-    </div>
-    <div class="containerSyria">
-      <div class="top">
-
-      </div>
-      <div class="right">
-
-      </div>
-      <div class="bottom">
-
-      </div>
-      <div class="left">
-
-      </div>
-    </div>
-    <div class="containerComicBook">
-      <div class="top">
-
-      </div>
-      <div class="right">
-
-      </div>
-      <div class="bottom">
-
-      </div>
-      <div class="left">
-
-      </div>
-    </div>
-
+    
+    <!-- call to JQUERY -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+    <!-- call to library for controlling GIF animation -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+    <script type="text/javascript" src="controllers/portfolio-controller.js"></script>
   </body>
 </html>
