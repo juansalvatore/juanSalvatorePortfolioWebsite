7eddec6343d017865741a9d11a0bca80a9de5f7a
Juan Salvatore <juansalvatore@live.com.ar>
2017-11-02 11:41:28 -0300

    Create portfolio container section

---
+++ portfolio.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="styles/portfolio-style.min.css">
+  </head>
+  <body>
+    <div class="containerBoole">
+      <div class="top">
+
+      </div>
+      <div class="right">
+
+      </div>
+      <div class="bottom">
+
+      </div>
+      <div class="left">
+
+      </div>
+    </div>
+    <div class="containerSudco">
+      <div class="top">
+
+      </div>
+      <div class="right">
+
+      </div>
+      <div class="bottom">
+
+      </div>
+      <div class="left">
+
+      </div>
+    </div>
+    <div class="containerLaNacion">
+      <div class="top">
+
+      </div>
+      <div class="right">
+
+      </div>
+      <div class="bottom">
+
+      </div>
+      <div class="left">
+
+      </div>
+    </div>
+    <div class="containerShortFilm">
+      <div class="top">
+
+      </div>
+      <div class="right">
+
+      </div>
+      <div class="bottom">
+
+      </div>
+      <div class="left">
+
+      </div>
+    </div>
+    <div class="containerSyria">
+      <div class="top">
+
+      </div>
+      <div class="right">
+
+      </div>
+      <div class="bottom">
+
+      </div>
+      <div class="left">
+
+      </div>
+    </div>
+    <div class="containerComicBook">
+      <div class="top">
+
+      </div>
+      <div class="right">
+
+      </div>
+      <div class="bottom">
+
+      </div>
+      <div class="left">
+
+      </div>
+    </div>
+
+  </body>
+</html>
